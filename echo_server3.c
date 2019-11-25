e64d19c1c702debd9805429f069ba22f43dba3aa
ykodera <yk.infotech45@gmail.com>
2019-11-25 23:42:50 +0900

    プロセス終了のためwait処理を追加．しかし1クライアントのwaitが終わるまで他のクライアントからの文字列を受信できない．

---
+++ echo_server3.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+int main (void){
+  int listenfd, connfd;
+  pid_t pid;
+  char buf[BUFSIZ];
+  int nbytes;
+  int SERVER_PORT = 10000;
+  struct sockaddr_in servaddr;
+  if ((listenfd = socket (AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror ("socket"); exit(1);
+  }
+  memset (&servaddr, 0, sizeof (servaddr));
+  servaddr.sin_len = sizeof(servaddr);
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(SERVER_PORT);
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  if (bind (listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+    perror("bind"); exit(1);
+  }
+  if (listen(listenfd, 10) < 0) {
+    perror("listen"); exit(1);
+  }
+
+  
+  for (;;){
+    if ((connfd = accept (listenfd, (struct sockaddr *)NULL, NULL)) < 0) {
+      perror ("accept"); exit(1);
+    }
+    fprintf(stderr, "accept\n");
+    pid = fork();
+    if (pid == -1) {
+      perror("fork"); exit(1);
+    }
+    fprintf (stderr, "fork: %d\n", pid);
+    if (pid == 0) { /*child */
+      fprintf(stderr, "child\n");
+      close(listenfd);
+      while ((nbytes = read (connfd, buf, sizeof(buf))) > 0) {
+        fprintf(stderr, "child: ");
+        fwrite(buf, 1, nbytes, stderr);
+        write(connfd, buf, nbytes);
+    }
+      close(connfd);
+      fprintf(stderr, "child: exit\n");
+      exit(0);
+    } else { /* parent */
+      int status; pid_t wpid;
+      //while ((nbytes = read (connfd, buf, sizeof (buf))) > 0 ){
+      //fwrite(buf,1, nbytes, stderr);
+      //write(connfd, buf, nbytes);
+      //  }
+    close(connfd);
+    fprintf(stderr, "parent\n");    
+    while ((wpid = waitpid (-1, &status, WNOHANG)) >= 0) {
+      // Do nothing
+      // -1:任意のプロセス WNOHANG:終了プロセスがなくても待たない
+    }
+    }
+  }
+}
